7c312eedaa430b0d4a6915d69f6e27f9ab903f56
Asaju Enitan Michael <asajuenitan@gmail.com>
2021-03-15 13:43:50 +0100

    ✍🏻 UPDATE(Start): Add start command

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-const http = require('axios').default;
-const TelegramBot = require('node-telegram-bot-api');
+const http = require("axios").default;
+const TelegramBot = require("node-telegram-bot-api");
 
 class StraeDog {
   token = process.env.TOKEN ? process.env.TOKEN : "";
   bot = new TelegramBot(this.token);
-  movieApiUrl = 'https://straedog-movie-server.herokuapp.com';
-  devUrl = 'http://localhost:8000'
+  movieApiUrl = "https://straedog-movie-server.herokuapp.com";
+  devUrl = "http://localhost:8000";
 
   startBot() {
     this.bot.startPolling();
@@ -17,37 +17,64 @@ class StraeDog {
     this.requestLink();
   }
 
+  start() {
+    this.bot.on("message", (msg) => {
+      const chatId = msg.chat.id;
+      if (message === "/start") {
+        this.bot.sendMessage(
+          chatId,
+          "Welcome to StraeDog Bot. \n Available Command /request <movie name>",
+          {
+            reply_to_message_id: msg.message_id,
+          }
+        );
+      }
+    });
+  }
+
   getHelp() {
     this.bot.onText(/\/help/, (msg, match) => {
       const chatId = msg.chat.id;
-      this.bot.sendMessage(chatId, "Available Command \n /request <Title> - Get movie download link", {
-        reply_to_message_id: msg.message_id,
-      });
+      this.bot.sendMessage(
+        chatId,
+        "Available Command \n /request <Title> - Get movie download link",
+        {
+          reply_to_message_id: msg.message_id,
+        }
+      );
     });
   }
-  
-async requestLink() {
+
+  async requestLink() {
     this.bot.onText(/\/request (.+)/, async (msg, match) => {
       const chatId = msg.chat.id;
       const name = match ? match[1] : "";
       const movieData = await this.getMovie(name);
-      this.bot.sendPhoto(chatId, `${movieData.poster}`, {
-        reply_to_message_id: msg.message_id
-      }).then((response) => {
-      console.log(movieData);
-        this.bot.sendMessage(chatId, `<b>${movieData.title}</b> \n <i>${movieData.genre}</i>
-		 \n ${movieData.link}`, {
-          reply_to_message_id: response.message_id,
-          parse_mode: 'html'
+      this.bot
+        .sendPhoto(chatId, `${movieData.poster}`, {
+          reply_to_message_id: msg.message_id,
         })
-      })
+        .then((response) => {
+          console.log(movieData);
+          this.bot.sendMessage(
+            chatId,
+            `<b>${movieData.title}</b> \n <i>${movieData.genre}</i>
+		 \n ${movieData.link}`,
+            {
+              reply_to_message_id: response.message_id,
+              parse_mode: "html",
+            }
+          );
+        });
     });
   }
 
   async getMovie(name) {
     try {
-      const response = await http.post(`${this.movieApiUrl}/get`, { name: name });
-      return response.data.data
+      const response = await http.post(`${this.movieApiUrl}/get`, {
+        name: name,
+      });
+      return response.data.data;
     } catch (err) {
       return err;
       console.error(err);
@@ -56,4 +83,3 @@ async requestLink() {
 }
 
 module.exports = StraeDog;
-
