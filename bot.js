357c25659e43aa76b02d1e9d5dbbe8e527cce5d1
Asaju Enitan Michael <asajuenitan@gmail.com>
2021-03-19 19:46:56 +0100

    ✍🏻 UPDATE():

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const http = require("axios").default;
 const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
 
 class StraeDog {
   token = process.env.TOKEN ? process.env.TOKEN : "";
@@ -15,20 +16,19 @@ class StraeDog {
   configureCommands() {
     this.getHelp();
     this.requestLink();
+    this.start();
   }
 
   start() {
-    this.bot.on("message", (msg) => {
+    this.bot.onText(/\/start/, (msg, match) => {
       const chatId = msg.chat.id;
-      if (message === "/start") {
-        this.bot.sendMessage(
-          chatId,
-          "Welcome to StraeDog Bot. \n Available Command /request <movie name>",
-          {
-            reply_to_message_id: msg.message_id,
-          }
-        );
-      }
+      this.bot.sendMessage(
+        chatId,
+        "Welcome to StraeDog Bot. \n Available Command /request <movie name>",
+        {
+          reply_to_message_id: msg.message_id,
+        }
+      );
     });
   }
 
@@ -49,35 +49,51 @@ class StraeDog {
     this.bot.onText(/\/request (.+)/, async (msg, match) => {
       const chatId = msg.chat.id;
       const name = match ? match[1] : "";
-      const movieData = await this.getMovie(name);
+      const movieData = await this.processData(
+        await this.getMovie(name)
+      );
       this.bot
-        .sendPhoto(chatId, `${movieData.poster}`, {
+        .sendPhoto(chatId, movieData.poster, {
           reply_to_message_id: msg.message_id,
         })
         .then((response) => {
-          console.log(movieData);
-          this.bot.sendMessage(
-            chatId,
-            `<b>${movieData.title}</b> \n <i>${movieData.genre}</i>
-		 \n ${movieData.link}`,
-            {
-              reply_to_message_id: response.message_id,
-              parse_mode: "html",
-            }
-          );
+          this.bot.sendMessage(chatId, movieData.result, {
+            reply_to_message_id: response.message_id,
+            parse_mode: "html",
+          });
         });
     });
   }
 
   async getMovie(name) {
     try {
-      const response = await http.post(`${this.movieApiUrl}/get`, {
-        name: name,
+      const response = await http.get(`${this.devUrl}/get`, {
+        params: {
+          name: name,
+        },
       });
       return response.data.data;
     } catch (err) {
-      return err;
-      console.error(err);
+      return {
+        statusCode: err.response.status,
+        message: err.response.statusText,
+      };
+    }
+  }
+
+  async processData(response) {
+    let result = "";
+    if (response.statusCode !== 404) {
+      const moviePoster = response.poster;
+      result = `<b>Title: ${response.title}</b> \n <i>Genre: ${response.genre}</i> \n ${response.link}`;
+      return { poster: moviePoster, result };
+    } else {
+      const moviePoster =
+        "https://firebasestorage.googleapis.com/v0/b/straedogbot.appspot.com/o/404.png?alt=media&token=396a1c04-11d3-44e1-abee-c106c6c23928";
+      result = `<b> I am sorry 😓</b> \n 
+      Movie not added yet. But I have sent a request on your behalf \n 
+      Be sure to check back`;
+      return { poster: moviePoster, result };
     }
   }
 }
